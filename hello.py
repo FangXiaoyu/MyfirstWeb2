5bc35080836260fdbf4b6a894491f8e6d48aa0d0
fangxiaoyu <505558491@qq.com>
2016-07-28 21:30:16 +0800

    5a

--- hello.py
+++ hello.py
@@ -1,13 +1,14 @@
 import os
-from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_script import Manager
-from flask_bootstrap import Bootstrap
-from flask_moment import Moment
-from flask_wtf import Form
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.script import Manager, Shell
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from flask_sqlalchemy import SQLAlchemy
-from flask_script import Shell
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail, Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,11 +17,21 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <505558491@qq.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class Role(db.Model):
@@ -43,13 +54,24 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+
 def make_shell_context():
-	return dict(app = app,db = db,User = User,Role = Role)
-manager.add_command("shell",Shell(make_context = make_shell_context))
+    return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -65,20 +87,21 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-    	user = User.query.filter_by(username = form.name.data).first()
-	if user is None:
-		user = User(username = form.name.data)
-		db.session.add(user)
-		session['known'] = False
-	else:
-		session['known'] = True
-	session['name'] = form.name.data
-	form.name.data = ' '
-	return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'),
-    known = session.get('known',False))
+                           known=session.get('known', False))
 
 
 if __name__ == '__main__':
-    db.create_all()
     manager.run()
